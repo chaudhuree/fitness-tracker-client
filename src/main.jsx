464a06b08d813d7a410cdb820f464774ec56aeb3
chaudhuree <chaudhuree00@gmail.com>
2024-06-09 01:49:29 +0600

    react router dom and tanstack query setup done.

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter as Router } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <HelmetProvider>
+        <App />
+        <Toaster position="top-right" />
+        </HelmetProvider>
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>
 );
